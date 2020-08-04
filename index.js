4ff791c0ad441041d3cc5f25ecbd8c3cbbc765a5
Czaroos <czaroosik@gmail.com>
2020-08-04 14:09:14 +0200

    basic structure for graphql server done

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+
+  }
+`;
+
+const resolvers = {
+  Query: {},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
