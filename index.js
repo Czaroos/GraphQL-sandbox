359d93c08f53f8ade620cf3808aece91d26390c7
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-08-05 09:03:09 +0200

    Resolvers moved to resolvers.js

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 const typeDefs = require('./schema');
-
-const resolvers = {
-  Date: new GraphQLScalarType({
-    name: 'Date',
-    description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value);
-    },
-    serialize(value) {
-      return value.getTime();
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return parseInt(ast.value, 10);
-      }
-      return null;
-    },
-  }),
-  MutationResponse: {
-    __resolveType(mutationResponse, context, info){
-      return null;
-    },
-  },
-};
+const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
---
+++ resolvers.js
@@ -0,0 +1,26 @@
+const { GraphQLScalarType } = require('graphql');
+const resolvers = {
+    Date: new GraphQLScalarType({
+      name: 'Date',
+      description: 'Date custom scalar type',
+      parseValue(value) {
+        return new Date(value);
+      },
+      serialize(value) {
+        return value.getTime();
+      },
+      parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+          return parseInt(ast.value, 10);
+        }
+        return null;
+      },
+    }),
+    MutationResponse: {
+      __resolveType(mutationResponse, context, info){
+        return null;
+      },
+    },
+  };
+  
+module.exports = resolvers;
