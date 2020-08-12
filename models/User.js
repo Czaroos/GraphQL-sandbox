ae530b58e7a54e2c608336cc87f50a6d0a3c436d
Czaroos <czaroosik@gmail.com>
2020-08-12 13:38:46 +0200

    user model created

--- models/User.js
+++ models/User.js
@@ -1,7 +1,17 @@
+const { setQuery } = require('../utils/queries');
+const { AuthenticationError } = require('apollo-server');
+
 const User = {
   getUser: async (token) => {
     const user = await setQuery(`SELECT * FROM "users" WHERE token=${token}`);
     return user;
+  },
+  getUser: async (email, password) => {
+    const user = await setQuery(`SELECT * FROM "users" WHERE email=${email}`);
+    if (!user) return new AuthenticationError('User doesnt exist in database');
+    if (user.password === password) return user;
+
+    return new AuthenticationError('User');
   },
 };
 
