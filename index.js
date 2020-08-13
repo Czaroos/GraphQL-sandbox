59d6bcbfcf1e4fcaeda1b4126c776826dd469633
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-08-13 10:49:44 +0200

    fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000`)
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
 
 client.connect((err) => {
